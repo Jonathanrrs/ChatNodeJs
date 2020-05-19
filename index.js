78062fbe31f04c52eea9169461933841f3c83a22
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-05-19 17:19:13 -0500

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+require('./public/sockets')(io);
+
+
+
+
+//Enviando archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+//Empezando el servidor
+server.listen(app.get('port'), () => {
+    console.log('Running on port', app.get('port'));
+    
+});
